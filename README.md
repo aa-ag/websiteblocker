ec71452da872590dedb38b1e354b1692572d0ca2
aa-ag <aaron.aguerrevere@gmail.com>
2021-01-04 18:19:51 -0600

    imports & global variables

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Python script to block a list of websites during a pre-set schedule.
 ### Architecture
 
 The script maps hostnames of websites to localhost address.
+
+## To run
+
+- `sudo nano /etc/hosts`
+- enter password
+- take note of your hosts
--- script.py
+++ script.py
@@ -2,10 +2,21 @@
 import time
 import datetime
 
+###--- GLOBAL VARIABLES ---###
+
+host = '/etc/hosts'
+
+redirect = '127.0.0.1'
+
+to_block = [
+    'www.facebook.com',
+    'facebook.com'
+]
 
 ###--- FUNCTION(S) ---###
 
-def block():
+
+def block(start_time, end_time):
     pass
 
 
